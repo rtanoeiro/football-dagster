73d817eee36e98498ac9006d18c572c8b1eb46c5
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2023-04-25 21:41:24 +1000

    Able to make resources work

--- footbal_dagster/resources/credentials.py
+++ footbal_dagster/resources/credentials.py
@@ -11,9 +11,7 @@ def get_credentials():
     """
     This function will be user to get the credentials to make API Requests
     """
-    api_key = os.environ["X-RAPIDAPI-KEY"]
-    api_host = os.environ["X-RAPIDAPI-HOST"]
-
-    api_credentials = {"X-RapidAPI-Key": api_key, "X-RapidAPI-Host": api_host}
-
-    return api_credentials
+    return {
+        "X-RapidAPI-Key": os.environ["X-RAPIDAPI-KEY"],
+        "X-RapidAPI-Host": os.environ["X-RAPIDAPI-HOST"],
+    }
